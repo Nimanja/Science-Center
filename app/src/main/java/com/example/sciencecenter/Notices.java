c99838ced60fd406990d255f5da84499218175f2
Nimanja <manoganimanja@gmail.com>
2019-07-24 02:43:58 +0530

    Sample UI.. get header and footer for your page

---
+++ app/src/main/java/com/example/sciencecenter/Notices.java
@@ -0,0 +1,14 @@
+package com.example.sciencecenter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Notices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notices);
+    }
+}
