10deaf68f331a84767f6247c28bb94e00e899b24
Nimanja <manoganimanja@gmail.com>
2019-08-06 19:51:15 +0530

    Add the fragment for my part (notices). this is my first time fragment used after I will do some changing.

--- app/src/main/java/com/example/sciencecenter/MainActivity.java
+++ app/src/main/java/com/example/sciencecenter/MainActivity.java
@@ -1,17 +1,27 @@
 package com.example.sciencecenter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.service.autofill.OnClickAction;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static int SPLASH_TIME_OUT = 5000; //This is 8 seconds
+
+    Button noticesBtn, classBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noticesBtn = findViewById(R.id.btnN);
+
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -21,5 +31,15 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         },SPLASH_TIME_OUT);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+
+            case R.id.btnN:
+                getSupportFragmentManager().beginTransaction().replace(R.id.defaultFragment, new NoticesHome()).commit();
+
+        }
     }
 }
