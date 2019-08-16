9b6172eed674e42c4cb712e5a1b8f996bfdbeaf1
Nimanja <manoganimanja@gmail.com>
2019-08-16 10:30:01 +0530

    Attendes Ui is there

--- app/src/main/java/com/example/sciencecenter/Attendees.java
+++ app/src/main/java/com/example/sciencecenter/Attendees.java
@@ -2,13 +2,36 @@ package com.example.sciencecenter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Attendees extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attendees);
+
+        button = findViewById(R.id.btnH);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Home.class);
+                startActivity(i);
+            }
+        });
+        button = findViewById(R.id.btnback);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Home.class);
+                startActivity(i);
+            }
+        });
     }
 }
