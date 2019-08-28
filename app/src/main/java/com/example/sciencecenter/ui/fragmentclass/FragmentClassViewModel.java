bc1c9a4a274f357202a665151195f14d6e8f18fb
Nimanja <manoganimanja@gmail.com>
2019-08-28 11:51:27 +0530

    Add the fragment for my part (notices). this is my first time fragment used after I will do some changing.

---
+++ app/src/main/java/com/example/sciencecenter/ui/fragmentclass/FragmentClassViewModel.java
@@ -0,0 +1,7 @@
+package com.example.sciencecenter.ui.fragmentclass;
+
+import androidx.lifecycle.ViewModel;
+
+public class FragmentClassViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
