2e06d3172ea6011e2149fe222a54ff3b095ddcfd
Nimanja <manoganimanja@gmail.com>
2019-09-14 10:40:49 +0530

    Feedback From is ok

---
+++ app/src/main/java/com/example/sciencecenter/NoticeFeedback.java
@@ -0,0 +1,44 @@
+package com.example.sciencecenter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
+import android.renderscript.Sampler;
+import android.view.ActionMode;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.Toast;
+
+public class NoticeFeedback extends AppCompatActivity {
+
+    Button backbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice_feedback);
+
+        backbtn = findViewById(R.id.btnback);
+
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(getApplicationContext(), NoticesHome.class);
+                startActivity(i);
+            }
+        });
+    }
+}
