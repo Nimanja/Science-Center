8afb28a8de3027cf396380d4998843192fe7c9b3
Nimanja <manoganimanja@gmail.com>
2019-09-17 14:53:33 +0530

    I'm delete all fragment activities.

--- app/src/main/java/com/example/sciencecenter/NoticeFeedback.java
+++ app/src/main/java/com/example/sciencecenter/NoticeFeedback.java
@@ -8,9 +8,6 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-
 public class NoticeFeedback extends AppCompatActivity {
 
 
@@ -19,15 +16,11 @@ public class NoticeFeedback extends AppCompatActivity {
     private EditText mmsgText;
     private Button msubbtn;
     private Button mcambtn;
-    private StorageReference mStorage;
-    private static final int GALLERY_INTENT = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notice_feedback);
-
-        mStorage = FirebaseStorage.getInstance().getReference();
 
         mcambtn = (Button) findViewById(R.id.cambtn);
         mfbnameText = (EditText) findViewById(R.id.fbnameText);
@@ -46,16 +39,5 @@ public class NoticeFeedback extends AppCompatActivity {
             }
         });
 
-        mcambtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_PICK);
-                intent.setType("image/*");
-                startActivityForResult(intent, GALLERY_INTENT);
-            }
-        });
-    }
-
-    protected void onCreate() {
     }
 }
